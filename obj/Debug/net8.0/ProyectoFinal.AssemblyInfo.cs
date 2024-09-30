13cd378119183629f7ce64926a74050b52adee5e
Android Studio <devandroid@ort>
2024-09-30 12:03:12 -0300

    cambios grodo

--- obj/Debug/net8.0/ProyectoFinal.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProyectoFinal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af1003ba44566252fd648eca5bfb3572127f1531")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+662541b33c2ea10599f34afb9ea6b48cb043cd86")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
